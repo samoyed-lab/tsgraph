79542aa76ece49e84fa32e9c6c47cd714a45e629
BuildTools <davidmaamoaix@gmail.com>
2023-02-03 00:36:25 +0800

    array functions decl

--- tsgraph/csrc/misc/ptr_array.h
+++ tsgraph/csrc/misc/ptr_array.h
@@ -20,13 +20,24 @@ typedef void (*PtrDestructor)(void *);
     dependents of a node in the computation graph (i.e. all nodes
     to forward-propagate changes to).
 
-    Field names are taken from glib's GRealPtrArray.
+    Fields and functions names are taken from glib's GRealPtrArray
+    for recognizability.
 */
 typedef struct {
-    void *data;
+    void *pdata;
     uint32_t len;
     uint32_t alloc;
     PtrDestructor element_free_func;
 } PtrArray;
+
+#define ptr_array_index(array, idx) ((array)->pdata)[idx];
+
+PtrArray *ptr_array_new(PtrDestructor element_free_func);
+
+void *ptr_array_remove_index(PtrArray *array, uint32_t idx);
+void *ptr_array_steal_index(PtrArray *array, uint32_t idx);
+
+void ptr_array_add(PtrArray *array, void *ptr);
+void ptr_array_insert(PtrArray *array, uint32_t idx, void *ptr);
 
 #endif // TSGRAPH__CSRC_PTR_ARRAY_H_
