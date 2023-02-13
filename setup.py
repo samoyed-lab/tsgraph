169b38506397ad41b9c4b2b70da282d4fc738c1a
BuildTools <davidmaamoaix@gmail.com>
2023-02-13 23:27:08 +0800

    Automatic library configuration when linking

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
+import pkgconfig
+
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
+
+backend = Extension(
+    'tsgraph.backend',
+    sources = [
+        'tsgraph/csrc/misc/ptr_array.c',
+
+        'tsgraph/csrc/graph/comp_graph.c',
+        'tsgraph/csrc/api/py_graph_impl.c',
+        'tsgraph/csrc/api/pyobj_operators.c',
+
+        'tsgraph/csrc/module.c'
+    ],
+    # libraries = ['glib-2.0'],
+    include_dirs = [
+        'tsgraph/csrc',
+        # '/usr/local/Cellar/glib/2.74.5/include/glib-2.0',
+        # '/usr/local/lib/glib-2.0/include'
+    ]
+)
+pkgconfig.configure_extension(backend, 'glib-2.0')
 
 setup(
     name = "tsgraph",
@@ -13,24 +35,6 @@ setup(
     },
 
     ext_modules=[
-    	Extension(
-            'tsgraph.backend',
-            libraries = ['glib-2.0'],
-            sources = [
-                'tsgraph/csrc/misc/ptr_array.c',
-
-                'tsgraph/csrc/graph/comp_graph.c',
-                'tsgraph/csrc/api/py_graph_impl.c',
-                'tsgraph/csrc/api/pyobj_operators.c',
-
-                'tsgraph/csrc/module.c'
-            ],
-            include_dirs = [
-                'tsgraph/csrc',
-                # TODO: automatic this library searching thing...
-                '/usr/local/Cellar/glib/2.74.5/include/glib-2.0',
-                '/usr/local/lib/glib-2.0/include'
-            ]
-        )
+    	backend
     ]
 )
