7747fb96a8b3d53a5f37d702da201e3b080d03f8
BuildTools <davidmaamoaix@gmail.com>
2023-03-20 12:54:06 +0800

    removed C backend

--- tsgraph/csrc/misc/macros.h
+++
@@ -1,9 +0,0 @@
-#ifndef TSGRAPH__CSRC_MACROS_H_
-#define TSGRAPH__CSRC_MACROS_H_
-
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-
-#define SetErr_NoMem PyErr_SetString(PyExc_MemoryError, "malloc failed")
-
-#endif // TSGRAPH__CSRC_MACROS_H_
